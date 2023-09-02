3afb38d856ea5fb34e35de9f57fa07780ac55bb4
cesarifuentes <fuentesivancesar@gmail.com>
2023-09-02 07:27:29 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # ReplaceMe
+
+## To Do
+
+- [ ] List Items
+- [ ] Create Item
+- [ ] Read Item
+- [ ] Update Item
+- [ ] Delete Item
+- [ ] Preset Items
+- [ ] Send Notifications
+- [ ] Limit Items
+- [ ] Purchase Subscription
+- [ ] iCloud Storage
---
+++ ReplaceMe/ReplaceMe/ReplaceMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReplaceMeApp.swift
+//  ReplaceMe
+//
+//  Created by Cesar Fuentes on 9/2/23.
+//
+
+import SwiftUI
+
+@main
+struct ReplaceMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
